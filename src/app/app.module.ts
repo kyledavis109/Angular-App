dc49b5cf24c76f0c06d58357c84d204106bac009
Kyle Davis <kyledavis109@gmail.com>
2022-08-14 18:26:45 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { EmailComponent } from './components/email/email.component';
+import { SendEmailComponent } from './components/send-email/send-email.component';
+
+const appRoutes: Routes = [
+  {path: '', component: SendEmailComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    EmailComponent,
+    SendEmailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
